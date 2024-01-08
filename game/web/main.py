4f66d81a879fed149a1a319c729d5dfe8caee3dc
Suthinan Rongphon <suthinan.rpn@gmail.com>
2024-01-08 21:18:24 +0700

    add class GameWidgat

--- game/web/main.py
+++ game/web/main.py
@@ -1,13 +1,35 @@
 import kivy
 
 from kivy.app import App
-from kivy.uix.label import Label
+from kivy.uix.widget import Widget
+from kivy.graphics import Rectangle
+from kivy.core.window import Window 
+
+class GameWidget(Widget):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        self._keyboard = Window.request_keyboard(self._on_keyboard_closed, self)
+        self._keyboard.bind(on_key_down = self._on_key_down)
+
+        with self.canvas:
+            Rectangle(
+                source = 'image/dinosaur.gif',
+                pos = (0, 0),
+                size = (228/2, 228/2)
+            )
+    def _on_keyboard_closed(self):
+        self._keyboard.unbind(on_key_down = self._on_key_down)
+        self._keyboard = None
+    
+    def _on_key_down(self, keyboard, keycode, text, modifiers):
+        print('key down', text)
+
+    
 
 class MyApp(App):
-
     def build(self):
-        return Label(text='')
-    
-    
+        return GameWidget()
+
 if __name__ == '__main__':
     MyApp().run()
