d21dfb2bc78360895e009e975f6f22b58eaccbd5
Suthinan Rongphon <suthinan.rpn@gmail.com>
2024-01-10 20:41:29 +0700

    add screen in class moneyApp

--- mamoney/web/main.py
+++ mamoney/web/main.py
@@ -11,8 +11,15 @@ class MaMoney(BoxLayout):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         
-    def login(self):
-        pass
+    def login(self, user, password):
+        user_app = {}
+        user_app['name'] = user
+        user_app['password'] = password
+        # self.user_list = []
+        # self.password_list = []
+class Login(BoxLayout):
+    
+
 
 class moneyApp(App):
     def build(self):
