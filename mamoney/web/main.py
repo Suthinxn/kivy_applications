6eabf1318769bb029fd1f53769e1816e9dc65795
Suthinan Rongphon <suthinan.rpn@gmail.com>
2024-01-10 19:47:23 +0700

    gui login

--- mamoney/web/main.py
+++ mamoney/web/main.py
@@ -1,15 +1,24 @@
+import kivy
+
 from kivy.app import App
-from kivy.uix.widget import Widget
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
+# from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
 
-class MaMoney(App):
-    pass
 
-class MaMoneyApp(App):
+class MaMoney(BoxLayout):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+    def login(self):
+        pass
+
+class moneyApp(App):
     def build(self):
         return MaMoney()
 
 
-
 if __name__ == "__main__":
-    MaMoneyApp().run()
-    
+    moneyApp().run()
+
