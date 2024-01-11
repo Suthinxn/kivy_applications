a4ec047c967c04394bfd1576e7a66d94a9c9960e
Suthinan Rongphon <suthinan.rpn@gmail.com>
2024-01-11 23:16:40 +0700

    recreate main.py

--- mamoney/web/main.py
+++ mamoney/web/main.py
@@ -1,4 +1,6 @@
 import kivy
+import random
+
 
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
@@ -8,62 +10,32 @@ from kivy.uix.textinput import TextInput
 from kivy.uix.screenmanager import ScreenManager, Screen
 
 
-user = []
-password = []
-
-class MaMoney(BoxLayout):
+class Mamoney(BoxLayout):
     def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        
-class Login(BoxLayout):
-    def login(self, user, password):
-        user_app = {}
-        user_app['name'] = user
-        user_app['password'] = password
-        # self.user_list = []
-        # self.password_list = []
+        self.text_greeting = ["Have a nice day!",
+                              "Good luck",
+                              "Have a wonderfulday!",
+                              "I wish today is a nice day for you"]
+        random_wish = random.choice(self.text_greeting)
+        print(random_wish)
 
 class Register(BoxLayout):
-    def __init__(self):
-        pass
 
-class Income_and_expense(BoxLayout):
-    def __init__(self):
-        pass    
+class Login(BoxLayout):
 
-class Compare_In_and_ex(BoxLayout):
-    def __init__(self):
-        pass
+class Home(BoxLayout):
+
+class Income_Expense(BoxLayout):
+
+class Save_money(BoxLayout):
 
 
 
-
-class moneyApp(App):
-    
-    
+class MamoneyApp(App):
     def build(self):
-        login_screen = Screen(name="")
-        income_expense_screen = Screen(name="")
-        register_screen = Screen(name="")
-        compare_screen = Screen (name="")
 
-        sm = ScreenManager()
-        
-        login_screen.add_widget(Login())
-        income_expense_screen.add_widget(Income_and_expense())
-        register_screen.add_widget(Register())
-        compare_screen.add_widget(Compare_In_and_ex())
 
-        box = BoxLayout(orientation = 'vertical')
-        header_label = Label(text="Login", font_size="60sp")
-        name_user = TextInput(font_size="30sp")
-        password_user = TextInput(font_size="30sp")
-        
-        
-
-        sm.current = "Log in"
-        return sm
 
 if __name__ == "__main__":
-    moneyApp().run()
+    MamoneyApp().run()
 
