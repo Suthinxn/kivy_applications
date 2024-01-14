a3fcce88fd6caa8fc2dba0f10e8cef05c13bf974
Suthinan Rongphon <suthinan.rpn@gmail.com>
2024-01-14 20:58:54 +0700

    add income expense

--- mamoney/web/main_app.py
+++ mamoney/web/main_app.py
@@ -1,6 +1,8 @@
 import kivy
 import random
 
+
+
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.core.window import Window
@@ -25,15 +27,18 @@ class Mamoney_app(Screen):
         print(self.random_greet)
         self.ids.greeting.text = self.random_greet
 
+global list_username, list_password
+list_username = []
+list_password = []
+
 class Register_user(Screen):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.list_username = ["Den"]
-        self.list_password = ["123456789"]
-    
+
     def check_infomation(self, username, password, confirm_password):
 
-        if username in self.list_username :
+        
+        if username in list_username :
             self.ids.topic.text = "This username is already taken!"
             print("This username is already taken!")
 
@@ -41,7 +46,7 @@ class Register_user(Screen):
             self.ids.topic.text = "Enter your name."
             print("Enter your name.")
 
-        elif len(password) < 6 :
+        elif len(password) < 8 :
             self.ids.topic.text = "Your password must be not less than 8 characters."
             print("Your password must be not less than 8 characters.") 
 
@@ -49,40 +54,53 @@ class Register_user(Screen):
             self.ids.topic.text = "Password don't match"
             print("Password don't match")
 
-        if username not in self.list_username:
+        if username not in list_username:
             if len(password) >= 6:
                 if confirm_password == password:
                     self.ids.topic.text = "Complete"
                     print("Complete")
 
-                    self.list_username.append(username)
-                    self.list_password.append(password)
+                    list_username.append(username)
+                    list_password.append(password)
                     print("Register succes")
-                    print(self.list_username)
-                    print(self.list_password)
+                    print(list_username)
+                    print(list_password)
+                
 
-    def database_usernaem(self):
-        return self.list_username 
-    def database_password(self):
-        return self.list_password
 class Login_user(Screen):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.class_register = Register_user()
-        self.list_username_login = self.class_register.database_usernaem()
-        self.list_password_login = self.class_register.database_password()
+        print("Login= username =>",list_username)
+        print("Login= password =>",list_password)
 
     def check_login(self, username_login, password_login):
-        if username_login in self.list_username_login:
-            index_user = self.list_username_login.index(username_login)
-            if password_login == self.list_password_login[index_user]:
+        if username_login in list_username:
+            index_user = list_username.index(username_login)
+            if password_login == list_password[index_user]:
                 self.ids.topic_login.text = "Login Succes"
                 print("Login Succes")
+                print("User", username_login)
+                print("Password", list_password[index_user])
             else:
                 self.ids.topic_login.text = "Invalid Password."
-                print("Tnvalid Password.")
-        print(self.list_username_login)
-        print(self.list_password_login)
+                print("Invalid Password.")
+
+
+class Save_money(Screen):
+    pass
+
+class Homepage(Screen):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.today = date.today()
+    def display_homepage(self, button_date):
+        self.ids.button_date.text = f"{self.today}"
+    
+class Income_expense(Screen):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+    pass
+
 
 kv = Builder.load_file("main.kv")
 
