173dd0185103c1a9e87a7335c929e1b6f45705df
Suthinan Rongphon <suthinan.rpn@gmail.com>
2024-01-14 15:23:24 +0700

    fix register

--- mamoney/web/main_app.py
+++ mamoney/web/main_app.py
@@ -20,14 +20,10 @@ class Mamoney_app(Screen):
                          "Wish you all the best.",
                          "Successfully pull off the heist!"]
         
-
     def random_greeting(self):
         self.random_greet = random.choice(self.greeting)
-
         print(self.random_greet)
         self.ids.greeting.text = self.random_greet
-
-
 
 class Register_user(Screen):
     def __init__(self, *args, **kwargs):
@@ -41,26 +37,41 @@ class Register_user(Screen):
             self.ids.topic.text = "This username is already taken!"
             print("This username is already taken!")
 
-        if len(password) < 6 :
+        elif len(username) == 0:
+            self.ids.topic.text = "Enter your name."
+            print("Enter your name.")
+
+        elif len(password) < 6 :
             self.ids.topic.text = "Your password must be not less than 8 characters."
             print("Your password must be not less than 8 characters.") 
 
-        if confirm_password != password:
+        elif confirm_password != password:
             self.ids.topic.text = "Password don't match"
             print("Password don't match")
 
         if username not in self.list_username:
-            self.ids.topic.text = "Complete"
-            print("Complete")
-            self.list_username.append(username)
-            self.list_password.append(password)
-            print(self.list_username)
-            print(self.list_password)
+            if len(password) >= 6:
+                if confirm_password == password:
+                    self.ids.topic.text = "Complete"
+                    print("Complete")
+
+                    self.list_username.append(username)
+                    self.list_password.append(password)
+                    print("Register succes")
+                    print(self.list_username)
+                    print(self.list_password)
+
+    def check_login(self):
+        pass
 
 class Login_user(Screen):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.class_register = Register_user()
+        
+        self.class_register.check_login()
         pass
+
 kv = Builder.load_file("main.kv")
 
 class main(App):
