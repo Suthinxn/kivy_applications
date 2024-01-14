04098de8f4e59257ccdf3f2d178890c2b5aa170a
Suthinan Rongphon <suthinan.rpn@gmail.com>
2024-01-14 14:38:07 +0700

    fix check register infomation

--- mamoney/web/main_app.py
+++ mamoney/web/main_app.py
@@ -2,14 +2,16 @@ import kivy
 import random
 
 from kivy.app import App
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.label import Label
-from kivy.uix.button import Button
-from kivy.uix.textinput import TextInput
+from kivy.lang import Builder
 from kivy.core.window import Window
+from kivy.uix.screenmanager import ScreenManager, Screen
+
 
 # Window.clearcolor = 1,1,0,1
-class Mamoney_app(BoxLayout):
+class WindowManager(ScreenManager):
+    pass
+
+class Mamoney_app(Screen):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.greeting = ["Have a nice day!",
@@ -24,12 +26,41 @@ class Mamoney_app(BoxLayout):
 
         print(self.random_greet)
         self.ids.greeting.text = self.random_greet
-   
 
+
+
+class Register_user(Screen):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.list_username = ["Den"]
+        self.list_password = []
+    
+    def check_infomation(self, username, password, confirm_password):
+
+        if username in self.list_username :
+            self.ids.topic.text = "This username is already taken!"
+            print("This username is already taken!")
+
+        if len(password) < 6 :
+            self.ids.topic.text = "Your password must be not less than 8 characters."
+            print("Your password must be not less than 8 characters.") 
+
+        if confirm_password != password:
+            self.ids.topic.text = "Password don't match"
+            print("Password don't match")
+
+        if username not in self.list_username:
+            self.ids.topic.text = "Complete"
+            print("Complete")
+class Login_user(Screen):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        pass
+kv = Builder.load_file("main.kv")
 
 class main(App):
     def build(self):
-        return Mamoney_app()
+        return kv
     
 
 if __name__ == '__main__':
